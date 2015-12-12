d23b9fb5dc46214a517b6b6aed84b745b8598773
gpascualg <gpascualg93@gmail.com>
2015-12-12 19:02:30 +0100

    Ability interface and abstract class

---
+++ LD34/Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+abstract class Ability : IAbility
+{
+    public abstract string Name { get; }
+    public abstract float CooldownTime { get; }
+
+    public virtual bool CanBeUsed { get { return RemainingCooldown == 0; } }
+
+    private float _remainingCooldown;
+    public float RemainingCooldown { get { return _remainingCooldown; } }
+
+    public virtual void OnEnded()
+    {
+    }
+
+    public virtual void OnEnable()
+    {
+        Assert.IsTrue(CanBeUsed);
+
+        _remainingCooldown = CooldownTime;
+    }
+
+    public virtual void Update()
+    {
+        _remainingCooldown -= Time.deltaTime;
+    }
+}
---
+++ LD34/Assets/Scripts/Abilities/IAbility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+interface IAbility
+{
+    string Name { get; }
+
+    bool CanBeUsed { get; }
+    float CooldownTime { get; }
+    float RemainingCooldown { get; }
+
+    void OnEnable();
+    void Update();
+    void OnEnded();
+}
