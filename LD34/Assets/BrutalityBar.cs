761f94dd6462e786e1525038a975fc8ac642d7de
4nc3str4l <cmuri_10@hotmail.com>
2015-12-14 00:41:08 +0100

    Brutality bar

---
+++ LD34/Assets/BrutalityBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BrutalityBar : MonoBehaviour {
+
+    Image barOverlay;
+
+    void Awake()
+    {
+        barOverlay = GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        barOverlay.fillAmount = (float)GameController.instance.numDeads / GameController.NUM_DEADS_TO_WIN;
+	}
+}
