e5cca71c7c37068cde6aff282493658f5a74cb2a
4nc3str4l <cmuri_10@hotmail.com>
2015-12-14 02:06:13 +0100

    Main Scene

---
+++ LD34/Assets/LoadingScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScript : MonoBehaviour {
+
+
+    public const float LETTER_SPAWN_RATE = 1f;
+    public Text text;
+    public string loadingText = "Loading...";
+    public string actualText = "Loading";
+    public float nextLetterSpawn = 0f;
+    int i = 7;
+
+    void Start()
+    {
+        Application.LoadLevel("C_GameScene");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(nextLetterSpawn < Time.time)
+        {
+            text.text = loadingText.Substring(0, i);
+            i++;
+            nextLetterSpawn = Time.time + LETTER_SPAWN_RATE;
+            if(i > loadingText.Length)
+            {
+                i = 7;
+            }
+        }
+	}
+}
