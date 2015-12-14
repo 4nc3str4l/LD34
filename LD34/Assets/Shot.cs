909905d9f390646a5f330e8e7e89f64e6601b09d
4nc3str4l <cmuri_10@hotmail.com>
2015-12-14 21:50:55 +0100

    Some more sounds

---
+++ LD34/Assets/Shot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+
+    // Use this for initialization
+    public AudioSource _source;
+
+    void OnEnable()
+    {
+        _source.PlayOneShot(_source.clip, 0.3f);
+    }
+}
