38c5fdf416b74d4e4f0cde8b9be93008b599fbb3
4nc3str4l <cmuri_10@hotmail.com>
2015-12-14 22:35:59 +0100

    New sounds

---
+++ LD34/Assets/Scripts/FXController/Aura.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Aura : MonoBehaviour {
+
+    AudioSource _audioSource;
+    bool firstPlay = true;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+	
+	void OnEnable()
+    {
+        if(firstPlay)
+        {
+            firstPlay = false;
+        }
+        else
+        {
+            _audioSource.PlayOneShot(_audioSource.clip);
+        }
+    }
+}
